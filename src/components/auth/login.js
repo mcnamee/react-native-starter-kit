51e29c47a779e7cf9dac5b8c90e17a5a8ccf6cb2
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-12-23 09:57:42 +1000

    Improve regex for email addresses

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -46,8 +46,9 @@ class Login extends Component {
     // Email Validation
     const validEmail = FormValidation.refinement(
       FormValidation.String, (email) => {
-        const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-        return re.test(email);
+        const regularExpression = /^.+@.+\..+$/i;
+
+        return regularExpression.test(email);
       }
     );
 

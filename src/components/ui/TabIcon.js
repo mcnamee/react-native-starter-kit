b675c5bd760a842d16419e89b7319dcb68b017d5
Philip Schneider <schneider.philip@hotmail.com>
2017-09-19 21:11:37 +0200

    fixed test

--- src/components/ui/TabIcon.js
+++ src/components/ui/TabIcon.js
@@ -13,21 +13,16 @@ import { Icon } from 'react-native-elements';
 import { AppColors } from '@theme/';
 
 /* Component ==================================================================== */
-const TabIcon = ({ icon, selected, type }) => (
+const TabIcon = ({ icon, selected }) => (
   <Icon
-    type={type}
     name={icon}
     size={26}
     color={selected ? AppColors.tabbar.iconSelected : AppColors.tabbar.iconDefault}
   />
 );
 
-TabIcon.propTypes = {
-  icon: PropTypes.string.isRequired,
-  selected: PropTypes.bool,
-  type: PropTypes.string,
-};
-TabIcon.defaultProps = { icon: 'search', selected: false, type: null };
+TabIcon.propTypes = { icon: PropTypes.string.isRequired, selected: PropTypes.bool };
+TabIcon.defaultProps = { icon: 'search', selected: false };
 
 /* Export Component ==================================================================== */
 export default TabIcon;

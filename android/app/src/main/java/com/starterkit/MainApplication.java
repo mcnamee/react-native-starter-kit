55b40d868202b67b08a77ad4401e517d87639d8a
starjet <starspark905.chz@gmail.com>
2017-10-11 12:18:53 +0530

    Remove RNDeviceInfo from MainApplication.java
    With this package no longer available, Android version will not build

--- android/app/src/main/java/com/starterkit/MainApplication.java
+++ android/app/src/main/java/com/starterkit/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new VectorIconsPackage(),
-            new GoogleAnalyticsBridgePackage(),
-            new RNDeviceInfo()
+            new GoogleAnalyticsBridgePackage()
       );
     }
   };

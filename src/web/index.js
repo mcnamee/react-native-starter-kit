7493b488a5943b8a21cf31909d1b693a70eaf79e
Matt Mcnamee <matt@mcnamee.co>
2017-11-22 13:51:33 +1000

    Add service worker

--- src/web/index.js
+++ src/web/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
 import configureStore from '../store/configureStore';
+import registerServiceWorker from './registerServiceWorker';
 
 // load our css
 require('./styles/style.less');
@@ -10,3 +11,4 @@ const store = configureStore();
 const rootElement = document.getElementById('root');
 
 render( <Root store={store} />, rootElement );
+registerServiceWorker();

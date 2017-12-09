9b96e5dcfffb5dd8e5abb916379f5fb37edb76ed
Matt Mcnamee <matt@mcnamee.co>
2017-12-09 15:37:21 +1000

    Simplify redux store

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Root from './src/native/containers/Root';
-import configureStore from './src/store/configureStore.prod.js';
+import configureStore from './src/store/configureStore.js';
 
 const store = configureStore();
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Root store={store} />
-    );
-  }
+export default function App() {
+  return <Root store={store} />;
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,12 @@
-if (process.env.NODE_ENV !== 'development' || process.env.PLATFORM_ENV !== 'web') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Reducers from '../reducers';
+
+// create a store that has redux-thunk middleware enabled
+const createStoreWithMiddleware = applyMiddleware(
+  thunk
+)(createStore);
+
+export default function configureStore() {
+  return createStoreWithMiddleware(Reducers);
 }

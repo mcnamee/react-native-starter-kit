353bb8a5b6f17f6ab796073076523cbfc175a600
Matt Mcnamee <matt@mcnamee.co>
2018-01-13 16:13:53 +1000

    Update script which auto-deploys to gh-pages

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-git subtree push --prefix build origin gh-pages
+npm run web-bundle;
+git add build/* && git commit -m 'Web JS bundled for GitHub Pages deploy';
+git subtree push --prefix build origin gh-pages;

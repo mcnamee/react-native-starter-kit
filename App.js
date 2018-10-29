cbd7ab5fc8178898cf76c7f0d6091ee799e6e079
tpps88206 <tpps88206@yahoo.com.tw>
2018-10-29 16:01:00 +0800

    Fixing fontFamily 'Ionicons' is not a system font

--- App.js
+++ App.js
@@ -4,6 +4,24 @@ import configureStore from './src/store/index';
 
 const { persistor, store } = configureStore();
 
-export default function App() {
-  return <Root store={store} persistor={persistor} />;
+export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Expo.Font.loadAsync({
+      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    if (!this.state.fontLoaded) {
+      return null;
+    }
+
+    return (<Root store={store} persistor={persistor} />);
+  }
 }

6e62b1709371079d92aae388cb6b0327a1261f88
Ertugrul Alemdar <art@atana.network>
2019-04-22 17:31:23 -0400

    Fix persist to work with rematch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,7 @@
 import member from './member';
 import recipes from './recipes';
 
-const rehydrated = (state = false, action) => {
-  switch (action.type) {
-    case 'persist/REHYDRATE':
-      return true;
-    default:
-      return state;
-  }
-};
-
 export default {
-  rehydrated,
   member,
   recipes,
 };

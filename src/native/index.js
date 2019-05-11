52acc3f2d8cf75aee4b45e16ab598e33dc6a17d2
Mihail Paley <undeadlol1@mail.ru>
2019-05-11 16:21:33 +0500

    fix firebase warnings

--- src/native/index.js
+++ src/native/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, Platform } from 'react-native';
+import { StatusBar, Platform, InteractionManager } from 'react-native';
 import { Font } from 'expo';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
@@ -15,6 +15,54 @@ import Loading from './components/UI/Loading';
 
 // Hide StatusBar on Android as it overlaps tabs
 if (Platform.OS === 'android') StatusBar.setHidden(true);
+
+/**
+ * Work around for recurring `Setting a timer for long time` warning.
+ * (Issue is related to "Firebase" package)
+ * see: https://github.com/firebase/firebase-js-sdk/issues/97
+ */
+if (Platform.OS === 'android') {
+  const _setTimeout = global.setTimeout;
+  const _clearTimeout = global.clearTimeout;
+  const MAX_TIMER_DURATION_MS = 60 * 1000;
+
+  const timerFix = {};
+  const runTask = (id, fn, ttl, args) => {
+    const waitingTime = ttl - Date.now();
+    if (waitingTime <= 1) {
+      InteractionManager.runAfterInteractions(() => {
+        if (!timerFix[id]) {
+          return;
+        }
+        delete timerFix[id];
+        fn(...args);
+      });
+      return;
+    }
+
+    const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+    timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+  };
+
+  global.setTimeout = (fn, time, ...args) => {
+    if (MAX_TIMER_DURATION_MS < time) {
+      const ttl = Date.now() + time;
+      const id = `_lt_${Object.keys(timerFix).length}`;
+      runTask(id, fn, ttl, args);
+      return id;
+    }
+    return _setTimeout(fn, time, ...args);
+  };
+
+  global.clearTimeout = (id) => {
+    if (typeof id === 'string' && id.startWith('_lt_')) {
+      _clearTimeout(timerFix[id]);
+      delete timerFix[id];
+      return;
+    }
+    _clearTimeout(id);
+  };
+}
 
 export default class App extends React.Component {
   static propTypes = {

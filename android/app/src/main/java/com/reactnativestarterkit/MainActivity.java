a922478f27b3f652919fddb8dd0f72ff47b90de2
mcnamee/react-native-boilerplate-builder <matt@mcnamee.co>
2020-02-26 05:50:55 +0000

    Builder Bot (mcnamee/react-native-boilerplate-builder)

--- android/app/src/main/java/com/reactnativestarterkit/MainActivity.java
+++ android/app/src/main/java/com/reactnativestarterkit/MainActivity.java
@@ -1,8 +1,16 @@
 package com.reactnativestarterkit;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+  // Show RN Splash Screen on launch
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- ios/ReactNativeStarterKit/AppDelegate.m
+++ ios/ReactNativeStarterKit/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -27,6 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
